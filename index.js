fe5b13be060fe380dc623dc77e2c5c5f081fc795
Eugene <eugenefromrus@gmail.com>
2018-11-17 19:52:05 +1000

    second lesson

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("hello");
+console.log(__dirname);
+console.log(__filename);
+var x = 0;
+if(x===0) {
+    console.log("work");
+}
+
+setInterval(function(){
+    console.log("hello")
+}, 2000);
