44e2180205bac17f6bebaa6fa86a32ba52dec719
Eugene <eugenefromrus@gmail.com>
2018-11-20 14:41:34 +1000

    sixth lesson

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 /*
-    Уроки Node JS / #3 - Функции, модули и директива require()
-    Именованные функции
-    модули
-    require
+    Уроки Node JS / #6 - Написание и чтение файлов
 */
 
-var things = require('./things');
+var fs = require('fs');
 
-var test = function() {
-    console.log("hello");
-}
 
-function call(func) {
-    func();
-}
+var file_readed = fs.readFile('text.txt', 'utf8', function(err, data) {
+    console.log(data)
+}); // прочитать содержимое из файла. Когда прочитан файл, тогда запустится нижестощий код, поэтому и называется Sync
 
-console.log(things.someval);
+console.log('test');
 
+fs.writeFile('some.txt', 'eee boy!!!', function(err, data){
+
+});
+
+console.log('test');
+
+// var file_write = fs.writeFileSync('some_new_file.txt', 'super porno website \n' + file_readed);
+// var file_readed2 = fs.readFileSync('some_new_file.txt', 'utf8'); // прочитать содержимое из файла. Когда прочитан файл, тогда запустится нижестощий код, поэтому и называется Sync
+
+
+// console.log(file_readed2);
